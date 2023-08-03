dc77114afc47c6343789e2a70ed021bcea946567
lim <limjung99@naver.com>
2023-08-03 16:53:19 +0900

    login 구현

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,AbstractUser
 # Create your models here.
 
-class User(User):
-    gender = models.CharField(null=False,max_length=10)
+class User(AbstractUser):
+    
+    email = models.CharField(null=False,max_length=50)
+    gender = models.CharField(null=False,max_length=20)
+    nickname = models.CharField(null=False,max_length=20)
+    birth = models.DateField(null=False)
+    password = models.CharField(null=False,max_length=20)
+
+    
     
--- authentication/serializer.py
+++ authentication/serializer.py
@@ -1,28 +1,33 @@
 from .models import User
 from django.contrib.auth.password_validation import validate_password 
+
 #serializer tools
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 from rest_framework.validators import UniqueValidator
 
-# Registration 시리얼라이저 및 검증 
+# 회원가입 시리얼라이저 클래스 
 class SignUpSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField( # 이메일 데이터 직렬화
+
+    class Meta:
+        model = User
+        fields = ['email','password','password_again','nickname','gender','birth','username']
+        
+
+    email = serializers.EmailField( 
         required = True,
         validators=[UniqueValidator(queryset=User.objects.all())] # 유효성 검사 
     )
-    password = serializers.CharField( # 문자열 데이터 직렬화
+
+    password = serializers.CharField( 
         required = True,
-        validators=[validate_password] # 일급객체
+        validators=[validate_password] 
     )
     password_again = serializers.CharField(
         write_only = True,
         required = True,
     )
-
-    class Meta:
-        model = User
-        fields = ['username','password','password_again','email']
 
     def validate(self,data):
         if data['password'] != data['password_again']:
@@ -30,13 +35,31 @@ class SignUpSerializer(serializers.ModelSerializer):
                 {"password":"비밀번호가 일치하지 않습니다."}
             )
         return data
+    
     def create(self,validated_data):
         user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email']
+            email=validated_data['email'],
+            nickname = validated_data['nickname'],
+            gender = validated_data['gender'],
+            birth = validated_data['birth'],
+            username= validated_data['username']
         )
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data['password']) # 해싱하여 password저장 
         user.save() # 저장
         token = Token.objects.create(user=user)
         return user
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+    # Validating
+    def validate(self,data):
+        user = authenticate(**data)
+        if user:
+            token = Token.objects.get(user=user) # 일치하는 유저의 토큰을 가져오기
+            return token
+        raise serializers.ValidationError(
+            {"error":"unalbe to authentication"}
+        )
 

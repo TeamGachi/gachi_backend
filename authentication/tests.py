c542707ab4347bf43743bfeb733bc272c4deeaf4
lim <limjung99@naver.com>
2023-09-09 21:12:42 +0900

    Testcases for TDD

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
-
+from rest_framework.test import APITestCase
+from rest_framework.views import status
 # Create your tests here.
+
+class LoginTestCase(APITestCase):
+
+    def setUp(self):
+        self.url_1 = '/api/authentication/login'
+        self.data = {
+            "email" : "testemail@naver.com",
+            "name" : "turing",
+            "gender" : "남자",
+            "birth" : "1999-08-22"
+        } 
+    
+    def test_normal_signup_case(self):
+        response = self.client.post(self.url_1, data=self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_anomaly_signup_case(self):
+
+        data = {
+            "email" : "testemailnaver.com",
+            "name" : "turing",
+            "gender" : "남자",
+            "birth" : "1999-08-22"
+        }
+        
+        response = self.client.post(self.url_1, data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
